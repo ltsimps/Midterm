7ce99ab158746af16946801cc2a03da2f1cd4a1c
ltsimps <ltsimpso@me.com>
2017-03-12 03:48:20 -0500

    Added License file and intitial test for input

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,16 @@
 #include <gtest/gtest.h>
-#include "lib.hpp"
+#include  <Parser.hpp>
 
 TEST(dummy, should_pass)
 {
     EXPECT_EQ(1, 1);
+}
+
+
+
+TEST(input, inputNonzero)
+{
+  Parser  p;
+
+    EXPECT_GT( p.getInput().size(), 0);
 }
